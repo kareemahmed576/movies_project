6f7107f32cafb063dce6fea4fcdbdcdbd4996956
kareemahmed576 <kareemfathy576@gmail.com>
2026-03-01 11:23:19 +0200

    Initial commit

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  static Color darkBlack = const Color(0xff121312);
+  static Color lightBlack = const Color(0xff1E1E1E);
+  static Color darkGray = const Color(0xff282A28);
+  static Color gold = const Color(0xffF6BD00);
+  static Color white = const Color(0xffffffff);
+  static Color green = const Color(0xff57AA53);
+  static Color red = const Color(0xffE82626);
+  static Color transparent =  Colors.transparent;
+}
